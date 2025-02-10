7a309defb2b6abaa380cd21eae0f96a44d6ab833
seemapoudel58 <simapoudel6@gmail.com>
2025-02-10 13:55:18 +0545

    Add input validation and improve user input handling in diabetes prediction app

--- app_diabetes.py
+++ app_diabetes.py
@@ -1,90 +1,95 @@
 import streamlit as st
 import pickle
 
+def validation(user_input ):
+    if user_input[0] < 0 or user_input[0] > 20:
+        return False
+    if user_input[1] < 0 or user_input[1] > 200:
+        return False
+    if user_input[2] < 0 or user_input[2] > 140:
+        return False
+    if user_input[3] < 0 or user_input[3] > 100:
+        return False
+    if user_input[4] < 0 or user_input[4] > 800:
+        return False
+    if user_input[5] < 0.00 or user_input[5] > 70.00:
+        return False
+    if user_input[6] < 0.000 or user_input[6] > 3.000:
+        return False
+    if user_input[7] < 0 or user_input[7] > 100:
+        return False
+    return True
+
 def app_diabetes():
     st.title('Diabetes Prediction using ML')
+    if not 'input_valid' in st.session_state:
+        st.session_state.input_valid = True
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
-
+        Pregnancies = st.number_input('Number of Pregnancies', value=2)
+   
     with col2:
-        Glucose = st.text_input('Glucose Level')
+        Glucose = st.number_input('Glucose Level ', value = 100 )
 
     with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
+        BloodPressure = st.number_input('Blood Pressure value', value= 70)
 
     with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
+        SkinThickness = st.number_input('Skin Thickness value', value = 35)
 
     with col2:
-        Insulin = st.text_input('Insulin Level')
+        Insulin = st.number_input('Insulin Level', value= 100)
 
     with col3:
-        BMI = st.text_input('BMI value')
+        BMI = st.number_input('BMI value', value = 32.00)
 
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+        DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function value', value=0.25, step=0.001, format="%.3f")
 
     with col2:
-        Age = st.text_input('Age of the Person')
+        Age = st.number_input('Age of the Person', value= 25)
 
     diab_diagnosis = ''
 
     test_accuracy = 0.753246
     diabetes_model = pickle.load(open('saved_models/diabetes_model_decision_tree.sav', 'rb'))
    
-
-    if st.button('Diabetes Test Result'):
-
-        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+    
+    user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
                       BMI, DiabetesPedigreeFunction, Age]
+    
+    st.session_state.input_valid = validation(user_input)
 
+    if not st.session_state.input_valid:
+        st.error('Please ensure all values are within their valid ranges before proceeding')
+
+    show_performance = False
+    
+    if st.button('Diabetes Test Result', disabled=not st.session_state.input_valid):
+        
+       
         user_input = [float(x) for x in user_input]
 
         diab_prediction = diabetes_model.predict([user_input])
 
         if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic using Decision Tree Model. The test accuracy of the model is ' + str(test_accuracy)  
+            diab_diagnosis = 'The person is diabetic using Decision Tree Model.'
         else:
-            diab_diagnosis = 'The person is not diabetic Decision Tree Model. The test accuracy of the model is ' + str(test_accuracy)
+            diab_diagnosis = 'The person is not diabetic using Decision Tree Model.' 
+        
+        show_performance = True
 
     st.success(diab_diagnosis)
 
-    if st.button('Show Model Performance'):
+    if show_performance:
+
         st.subheader("Model Performance on Test Data")
+        col1, col2 = st.columns([7,3.27])
+        with col1:
+            st.image('metrics_plot.png')
 
+        with col2:
+            st.image('confusion_matrix_custom.png')
 
-#     # Accuracy Score
-#         accuracy = accuracy_score(y_test, y_pred)
-#         st.write(f"**  Model Accuracy:** {accuracy:.2f}")
-
-#     # Classification Report
-#         st.write(" **Classification Report:**")
-
-#     #   Convert the classification report into a DataFrame
-#         report_dict = classification_report(y_test, y_pred, output_dict=True)
-
-#    # Extract relevant data
-#         report_df = pd.DataFrame(report_dict).transpose()
-#         report_df = report_df.rename(index={'0': 'No Disease (0)', '1': 'Disease (1)'})
-
-#    # Round values for better readability
-#         report_df = report_df[['precision', 'recall', 'f1-score', 'support']].round(2)
-
-#    # Display as a Streamlit table
-#         st.dataframe(report_df)
-
-
-
-#     # Confusion Matrix
-#         st.write(" **Confusion Matrix:**")
-#         conf_matrix = confusion_matrix(y_test, y_pred)
-
-#     # Plot Confusion Matrix
-#         fig, ax = plt.subplots(figsize=(5, 3))
-#         sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=['No Disease', 'Disease'], yticklabels=['No Disease', 'Disease'])
-#         plt.xlabel("Predicted")
-#         plt.ylabel("Actual")
-#         st.pyplot(fig)
