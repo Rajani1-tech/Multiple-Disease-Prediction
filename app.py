b4b6f0b9beb2516d39ae09b79f9c4db4ddf0b3f9
Predator <rajanilami.12@gmail.com>
2025-03-04 13:33:48 +0545

    connect login system on the app file

--- app.py
+++ app.py
@@ -1,28 +1,52 @@
 import streamlit as st
+from user import login, sign_up
 from streamlit_option_menu import option_menu
 from app_diabetes import app_diabetes
 from app_heart import app_heartdisease, model
 from app_breast_cancer import app_breast_cancer
 
+
+# Set page config at the top before any other Streamlit command
 st.set_page_config(page_title="Health Assistant",
                    layout="wide",
                    page_icon="🧑‍⚕️")
 
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+    
 
-with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',
+# Your database functions here...
 
-                           ['Diabetes Prediction', 'Heart Disease Prediction', 'Breast Cancer Prediction'
-                            ],
-                           menu_icon='hospital-fill',
-                            icons=['activity', 'heart', 'person'],
-                           default_index=1) 
+def load_health_assistant():
+    """Health Assistant Page"""
+    with st.sidebar:
+        selected = option_menu('Multiple Disease Prediction System',
+                               ['Diabetes Prediction', 'Heart Disease Prediction', 'Breast Cancer Prediction'],
+                               menu_icon='hospital-fill',
+                               icons=['activity', 'heart', 'person'],
+                               default_index=1)
 
+    if selected == 'Diabetes Prediction':
+        app_diabetes()
+    elif selected == 'Heart Disease Prediction':
+        app_heartdisease(model)
+    elif selected == 'Breast Cancer Prediction':
+        app_breast_cancer()
 
-if selected == 'Diabetes Prediction':
-    app_diabetes()    
-if selected == 'Heart Disease Prediction':
-    app_heartdisease(model)
-elif selected =='Breast Cancer Prediction':
-    app_breast_cancer()
+def main():
+    """Main Function to Display SignUp/Login"""
+    # Check if user is logged in
+    if not  st.session_state.logged_in:
+        st.title("welcome to Health Assistant 🏥")
+        
+        choice = st.radio("Choose an option", ['Sign Up', 'Login'])
 
+        if choice == 'Sign Up':
+            sign_up()
+        elif choice == 'Login':
+            login()
+    else:
+        load_health_assistant()        
+
+if __name__ == "__main__":
+    main()
